8d45b27fef962e605bf54f8afaf3c38c6e082aa7
saradeveloper92 <sara.elifani92@gmail.com>
2021-09-16 12:00:56 +0200

    modificata stampa parole in lista

--- arrayJs.js
+++ arrayJs.js
@@ -1,20 +1,22 @@
-var arrayParole = [];
+const arrayParole = [];
 
 function inputParole() {
     //prendo in input testo
-    var parole = testo.value;
+    let parole = testo.value.split(" ");
     //aggiungo elemento dentro array
     arrayParole.push(parole);
-    inserisciParoleInArray();
+    visualizzaParoleInArray();
     //svuoto campo di input
     testo.value = "";
 }
 
-function inserisciParoleInArray() {
-    var listaParole = "";
+function visualizzaParoleInArray() {
+    const inizio = "<ol>\n";
+    const fine = "\n</ol>";
+    let listaParole = "";
     //ciclo array parole 
     for (i = 0; i < arrayParole.length; i++) {
-        listaParole += " " + arrayParole[i];
+        listaParole += "<li>" + arrayParole[i] + "</li>";
     }
     //stampo risultato
     stampa.innerHTML = listaParole;
